9d7999eb5af28ff69624d3b1f1a1d64c9a7317b8
FaizanFazal12 <fazfaizan22@gmail.com>
2023-08-14 14:37:38 +0500

    final the projects

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Cart from "./component/Cart";
 import Banner from "./component/Banner";
 import Footer from "./component/Footer";
 import Payment from "./component/Payment";
+import Login from "./component/Login";
+import Regiser from "./component/Regiser";
 
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
         <Routes>
         <Route exact path="/" element={<Banner/>}/> 
         <Route exact path="/product" element={<Product />}/> 
+        <Route exact path="/login" element={<Login/>}/> 
+        <Route exact path="/register" element={<Regiser/>}/> 
           <Route exact path="/productitem/:id" element={<ProductItem />} />
           <Route exact path="/cart" element={<Cart/>} />
           <Route exact path="/payment" element={<Payment/>} />
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 export default function Home() {
 
   const cartLength = useSelector((state) => state.cart.products.length);
-
+const navigate=useNavigate()
   return (
     <div>
         <header className="text-gray-600 body-font">
@@ -18,19 +18,19 @@ export default function Home() {
     <nav className="md:mx-auto flex flex-wrap items-center text-base justify-center">
       <Link to="/" className="mr-5 hover:text-gray-900 cursor-pointer">Home</Link>
       <Link to="/product" className="mr-5 hover:text-gray-900 cursor-pointer">Product</Link>
-      <Link className="mr-5 hover:text-gray-900 cursor-pointer">About Us</Link>
+      <Link  className="mr-5 hover:text-gray-900 cursor-pointer">About Us</Link>
       <Link className="mr-5 hover:text-gray-900 cursor-pointer">Fourth Link</Link>
     </nav>
-    <button className="inline-flex items-center mx-2 bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
+    <Link to="/login" className="inline-flex items-center mx-2 bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
-    </button>
-    <button className="inline-flex items-center mx-2 bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Registered
+    </Link>
+    <Link to="/register"  className="inline-flex items-center mx-2 bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Registered
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
-    </button>
+    </Link>
     <Link to="/cart" className="inline-flex items-center mx-2 bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Cart ({cartLength})
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
